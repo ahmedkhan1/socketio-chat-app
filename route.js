b09bb64b88eb963e7a90c4f05b95e034fc1bb743
Ahmed khan <ahmed_khan019@hotmail.com>
2023-10-27 01:14:26 +0500

    Fixed formatting

--- route.js
+++ route.js
@@ -1,19 +1,19 @@
 const app = express();
 
 app.get('/messages', (req, res) => {
-    Message.find({},(err, messages)=> {
-      res.send(messages);
-    })
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
 });
 
 
 app.post('/messages', (req, res) => {
-    var message = new Message(req.body);
-    message.save((err) =>{
-      if(err)
-        sendStatus(500);
-      
-      io.emit('message', req.body);
-      res.sendStatus(200);
-    })
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
 })
