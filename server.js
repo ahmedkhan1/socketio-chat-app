c24c0107cc3d126357d7a364202768799430caf5
Ahmed khan <ahmed_khan019@hotmail.com>
2023-10-27 01:15:01 +0500

    Fixed inverted commas issue

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require("http’).Server(app);
-const io = require("socket.io’)(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const express = require('express');
 const app = express();
 
-const bodyParser = require(‘body-parser’)
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
