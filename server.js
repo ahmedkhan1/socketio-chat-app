8fc191a27f3a26493b88b84c5c2ce9c7e4dbcb50
Ahmed khan <ahmed_khan019@hotmail.com>
2023-10-29 22:17:53 +0500

    Fixed issues

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
 const express = require('express');
 const app = express();
 
@@ -13,6 +12,4 @@ const server = app.listen(3000, () => {
     console.log('server is running on port', server.address().port);
 });
 
-io.on("connection", () =>{
-    console.log("a user is connected")
-})
+
