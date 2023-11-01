25ec1edacd2ed04a6c6c85fd89d6e52a17eafe59
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-01 22:52:51 +0500

    Fixed sockets issue

--- socket.js
+++ socket.js
@@ -1,6 +1,9 @@
 const http = require("http").Server(app);
-export const io = require("socket.io")(http);
+const io = require("socket.io")(http);
 
 io.on("connection", () =>{
     console.log("a user is connected")
 })
+
+
+module.exports = io;
