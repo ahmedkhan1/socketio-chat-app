1dff437aa4044a1638d485f2d63a605a7b252502
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-01 23:05:50 +0500

    Added comments for routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const messageRoute = require("./messages.routes");
 const router = express.Router();
 
-
+// Default routes for the app
 const defaultRoutes = [
     {
         path: "messages",
