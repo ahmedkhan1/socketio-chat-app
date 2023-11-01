3fc1670d27f44e26609ea4771e0d5583081e93d2
Ahmed khan <ahmed_khan019@hotmail.com>
2023-11-01 23:13:11 +0500

    Fixed paranthesis

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
-const mongoose = require(mongoose’);
+const mongoose = require("mongoose");
 
 const dbUrl = "mongodb://username:pass@ds257981.mlab.com:57981/simple-chat";
 mongoose.connect(dbUrl , (err) => { 
     console.log("mongodb connected",err);
 })
 
-const Message = mongoose.model(‘Message’,{ name : String, message : String})
+const Message = mongoose.model("Message",{ name : String, message : String})
